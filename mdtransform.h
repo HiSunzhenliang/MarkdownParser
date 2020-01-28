343cee10be5da2782c8bab10f5c687f7811e6b59
HiSunzhenliang// <sunzhenliang@njust.edu.cn>
2020-01-28 10:46:14 +0800

    Update mdtransform.h

--- mdtransform.h
+++ mdtransform.h
@@ -13,7 +13,7 @@ class MarkdownTransform {
 public:
 	
 
-	std::string getTableOfContens()
+	std::string getTableOfContents()
 	{
 		throw std::logic_error("The method or operation is not implemented.");
 	}
